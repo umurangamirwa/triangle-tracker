6f1cd03e17253a0718840907433e665e57f77696
wecode <umurangamclaire@gmail.com>
2018-12-14 16:18:24 +0200

    add js

---
+++ triangle.js
@@ -0,0 +1,17 @@
+function getResult(){
+    var side1= parseFloat(document.getElementById('side1').value);
+    var side2 = parseFloat(document.getElementById('side2').value);
+    var aside3 = parseFloat(document.getElementById('side3').value);
+    if(side1 === side2 && side1 === side1 && side2 === side3){
+    alert("Equilateral");
+    }
+    else if(side1 === side2 || side1 === side3 ||side2 === side3){
+    alert("Isosceles");
+    }
+    else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
+    alert("Not a definite triangle");
+    }
+    else{
+    alert("Scalene");
+    }
+    }
