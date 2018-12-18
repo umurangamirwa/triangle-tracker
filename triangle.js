c103ac7e4c4771c2533ee80f160386c6b5c6413d
wecode <umurangamclaire@gmail.com>
2018-12-18 18:22:18 +0200

    add array

--- triangle.js
+++ triangle.js
@@ -1,3 +1,4 @@
+var sideOfTriangle=["side1","side2","side3"];
 function getResult(){
     var side1= parseFloat(document.getElementById('side1').value);
     var side2 = parseFloat(document.getElementById('side2').value);
