6ce4c7b3fe05d99330f2d56f337b189fb51160e9
wecode <umurangamclaire@gmail.com>
2018-12-18 12:29:56 +0200

    add changes in js

--- triangle.js
+++ triangle.js
@@ -1,8 +1,8 @@
 function getResult(){
     var side1= parseFloat(document.getElementById('side1').value);
     var side2 = parseFloat(document.getElementById('side2').value);
-    var aside3 = parseFloat(document.getElementById('side3').value);
-    if(side1 === side2 && side1 === side1 && side2 === side3){
+    var side3 = parseFloat(document.getElementById('side3').value);
+    if(side1 === side2 && side1 === side3 && side2 === side3){
     alert("Equilateral");
     }
     else if(side1 === side2 || side1 === side3 ||side2 === side3){
